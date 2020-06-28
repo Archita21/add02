314d6d2c66d8e0a57c55fdfa57b3e50d9ed04288
Archita21 <35433234+Archita21@users.noreply.github.com>
2020-06-28 20:14:12 +0530

    Add files via upload

---
+++ Input02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Input02
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the num1");
+
+int num1 = sc.nextInt();
+
+
+System.out.println("Enter the num2");
+int num2 = sc.nextInt();
+
+int num3 = num1 + num2;
+
+System.out.println("the sum is  num3=" +num3);
+}
+}
+
